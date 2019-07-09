7d154f8fd8d4a45cb51f040e1400a2c0c65fe460
vcaquilpan <victor.caquilpan@sma.gob.cl>
2019-07-09 17:48:05 -0400

    Subida de codigo en desktop

---
+++ main.js
@@ -0,0 +1,6 @@
+//Esta funcion permite que corra una fnción al momento que la pagina del documento esté lista.
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+
---
+++ mapa.js
@@ -0,0 +1,12 @@
+// Definir atributos iniciales para el Mapa
+const MAP_ZOOM = 10
+const MAP_CENTER = [-33.4444, -70.6535]
+
+// Crear instancia del Mapa
+var map = L.map('myMap').setView(MAP_CENTER, MAP_ZOOM)
+
+// Crear capa de sectores y Copyright del Mapa
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map)
+
