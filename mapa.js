0afc62adb052c26552de778577ffb7d039aebe12
vcaquilpan <victor.caquilpan@sma.gob.cl>
2019-07-11 11:10:55 -0400

    color a marcadores

--- mapa.js
+++ mapa.js
@@ -2,6 +2,34 @@
 const MAP_ZOOM = 10
 const MAP_CENTER = [-33.5544, -70.6535]
 
+// Se definen los iconos a utilizar
+var greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var blueIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 // Crear instancia del Mapa
 var map = L.map('myMap').setView(MAP_CENTER, MAP_ZOOM)
 
@@ -23,61 +51,24 @@ function MostrarDato(feature, layer) {
     }
   }
   
-
-  var greenIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
-
-
-  // Se agrega data al Mapa
-  /* d3.json('./Datos/Equipos.json')
-    .then((geojson) => {
-      L.geoJSON(geojson, {
-        onEachFeature: MostrarDato
-      }).addTo(map)
-    }
-
-) */
-
    d3.json('./Datos/Equipos.json')
     .then((geojson) => {
       L.geoJSON(geojson, {
-        onEachFeature: MostrarDato
+        onEachFeature: MostrarDato,
+        pointToLayer: function (geoJsonPoint,latlng){
+         switch (geoJsonPoint.properties.sensor_mp){
+           case 'PM2005':
+           return L.marker(latlng,{icon: redIcon})
+           case 'PMS3003':
+           return L.marker(latlng,{icon: greenIcon})
+           case 'PPD42':
+           return L.marker(latlng,{icon: blueIcon})
+         }
+        }
       }).addTo(map)
     }
 
 )
 
- /* --------------------------------  */
-
- /*
-L.circle([-33.5544, -70.6535],{
-    color:'red',
-}).addTo(map)
-.bindPopup('Prueba')
-
-
-  L.marker([-33.4544, -70.6535], {icon: greenIcon}).addTo(map);
-
-L.geoJSON(equipos, {
-    style: function(feature) {
-        switch (feature.properties.sensor_mp) {
-            case 'PMS3003' : return {icon:greenIcon}
-            
-           // case 'PPD42': return {color: "#ff1010"};
-           // case 'PMS3003':return {color: "#0000ff"};
-           // case 'PM2005': return {color: "#0000ff"};
-        }
-    }
-}).addTo(map);
-
-
-
-*/
-
+ 
 
